714f8d69562c416aaf28cde39c4b90d8c1b2bdd9
Niclas Schmitz <17707511+NiclasSchmitz@users.noreply.github.com>
2024-07-22 16:15:17 +0200

    Add yt-dlp aliases

--- zsh-custom/alias.zsh
+++ zsh-custom/alias.zsh
@@ -8,3 +8,7 @@ alias journal="cd $HOME/Documents/journal && vim ."
 alias today="open_journal_today"
 alias yesterday="open_journal_yesterday"
 alias tomorrow="open_journal_tomorrow"
+
+# yt-dlp
+alias yt-mp3="yt-dlp --extract-audio --audio-format 'mp3' --audio-quality 0"
+alias yt-video="yt-dlp -f bestvideo+bestaudio"
